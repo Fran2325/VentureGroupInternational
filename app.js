c8e7710c990a89e88497f192e1611f24e098b5ab
fran2325 <francismartinez2325@gmail.com>
2022-09-20 22:52:33 -0300

    Haciendo el responsive

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const grande    = document.querySelector('.grande')
 const punto     = document.querySelectorAll('.punto')
 
@@ -28,6 +27,40 @@ punto.forEach( ( cadaPunto , i )=> {
         })
         // Añadir la clase activo en el punto que hemos hecho CLICK
         punto[i].classList.add('activo')
+
+    })
+})
+
+
+const testimoniogrande    = document.querySelector('.testimoniogrande')
+const testimoniopunto     = document.querySelectorAll('.testimoniopunto')
+
+// Cuando CLICK en punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // QUITAR la clase activo de TODOS puntos
+    // AÑADIR la clase activo al punto que hemos hecho CLICK
+
+// Recorrer TODOS los punto
+testimoniopunto.forEach( ( cadaPunto , i )=> {
+    // Asignamos un CLICK a cadaPunto
+    testimoniopunto[i].addEventListener('click',()=>{
+
+        // Guardar la posición de ese PUNTO
+        let posicion  = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * -50
+
+        // MOVEMOS el grand
+        testimoniogrande.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos TODOS los punto
+        testimoniopunto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            testimoniopunto[i].classList.remove('testimonioactivo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        testimoniopunto[i].classList.add('testimonioactivo')
 
     })
 })
